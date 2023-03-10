03053b57722bbb9edb4f23c672cd3a233c000a8e
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-10 09:56:37 +0900

    reorganized code with independent sample app

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,6 @@ export default defineConfig({
   define: { "process.env": {} },
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
