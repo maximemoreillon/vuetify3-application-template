13de4448be3e2a9f2448fc5f4e049ed3dfec2c0a
Maxime Moreillon <m.moreillon@gmail.com>
2023-04-10 07:06:14 +0900

    minor

--- src/layouts/appTemplate/LoginForm.vue
+++ src/layouts/appTemplate/LoginForm.vue
@@ -63,8 +63,6 @@ const login = async () => {
     // @ts-ignore
     VueCookies.set("jwt", jwt);
 
-    // TODO: consider setting axios headers here
-
     await actions.getUser();
   } catch (error: any) {
     snackbar.show = true;
