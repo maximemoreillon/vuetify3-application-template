3866080f273a346acfd6ce327a89d13e378d2d76
Maxime Moreillon <m.moreillon@gmail.com>
2024-09-22 08:34:43 +0900

    added oidc support

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,12 @@ import { ref } from "vue";
 const options = ref({
   title: "Example application",
   author: "Maxime Moreillon",
-  login_url: "https://api.users.maximemoreillon.com/auth/login",
-  identification_url: "https://api.users.maximemoreillon.com/users/self",
+  // login_url: "https://api.users.maximemoreillon.com/auth/login",
+  // identification_url: "https://api.users.maximemoreillon.com/users/self",
   username_hint: "Test hint for username",
   password_hint: "Test hint for password",
+  oidc_authority: import.meta.env.VITE_OIDC_AUTHORITY,
+  oidc_client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
   // footer: false,
 });
 </script>
--- src/templateStore.ts
+++ src/templateStore.ts
@@ -2,6 +2,7 @@
 // TODO: use Pinia
 import { reactive } from "vue";
 import VueCookies from "vue-cookies";
+import OidcAuth from "@moreillon/oidc-auth";
 
 // Note: This axios is not that used by the main app
 import axios from "axios";
@@ -23,27 +24,62 @@ export const state = reactive<any>({
   authenticating: false,
 });
 
+const getUserLegacy = async () => {
+  // Note: token could be passed as parameter
+  try {
+    state.authenticating = true;
+
+    // TODO: Look for token in LocalStorage if needed too
+    // @ts-ignore
+    const jwt = VueCookies.get(jwtKey);
+
+    const { identification_url } = state.options;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+    const { data } = await axios.get(identification_url);
+
+    state.user = data;
+  } catch (error) {
+    console.error(error);
+    state.user = null;
+    delete axios.defaults.headers.common["Authorization"];
+  } finally {
+    state.authenticating = false;
+  }
+};
+
+const getUserOidc = async () => {
+  state.authenticating = true;
+
+  const { oidc_authority, oidc_client_id } = state.options;
+
+  const oidcOptions = {
+    authority: oidc_authority,
+    client_id: oidc_client_id,
+  };
+
+  const auth = new OidcAuth(oidcOptions);
+  auth.init().then((user) => {
+    if (!user) return;
+    state.user = user;
+  });
+
+  auth.userManager.events.addUserLoaded((user) => {
+    state.user = user;
+  });
+};
+
 export const actions = {
   async getUser() {
-    // Note: token could be passed as parameter
-    try {
-      state.authenticating = true;
+    const { identification_url, oidc_authority, oidc_client_id } =
+      state.options;
 
-      // TODO: Look for token in LocalStorage if needed too
-      // @ts-ignore
-      const jwt = VueCookies.get(jwtKey);
-
-      const { identification_url } = state.options;
-      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
-      const { data } = await axios.get(identification_url);
-
-      state.user = data;
-    } catch (error) {
-      console.error(error);
-      state.user = null;
-      delete axios.defaults.headers.common["Authorization"];
-    } finally {
-      state.authenticating = false;
+    if (oidc_authority && oidc_client_id) {
+      console.log("Getting user with OIDC");
+      await getUserOidc();
+    } else if (identification_url) {
+      await getUserLegacy();
+    } else {
+      console.error(`No authentication strategy available`);
     }
   },
 
